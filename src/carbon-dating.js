1b019830a2b215b18ea5ce0baad36ef22d5beba2
OrkasST <egor31@outlook.com>
2023-04-14 12:13:43 +0300

    feat: carbon-dating done

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,8 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  return Math.ceil(Math.log(MODERN_ACTIVITY/parseInt(sampleActivity)) / (Math.log(2) / HALF_LIFE_PERIOD))
 }
 
 module.exports = {
