d846865f7078557a77c5363e3a686c32e9342ae0
OrkasST <egor31@outlook.com>
2023-04-14 11:19:59 +0300

    init: started js-basic task

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,12 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
+function countCats(matrix) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let cats = 0;
+  matrix.forEach(x => x.forEach(y => y === '^^' ? cats++ : null));
+  return cats;
 }
 
 module.exports = {
