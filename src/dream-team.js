dfc94c32fa30e32ea7cb781196a6cef545d1f5e3
OrkasST <egor31@outlook.com>
2023-04-14 12:49:19 +0300

    feat: carbon-dating done

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,10 +13,32 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if (!Array.isArray(members)) return false;
+  return members.map(el => {
+    if (typeof el !== 'string') return '';
+    el = el.replace(/ /g,'');
+    console.log(el);
+    return el[0].toUpperCase();
+  }).sort().join('');
 }
+
+// console.log(createDreamTeam(
+//   [
+//     '   William Alston ',
+//     ' Paul Benacerraf',
+//     '  Ross Cameron',
+//     '       Gilles Deleuze',
+//     '  Arda Denkel ',
+//     '  Michael Devitt',
+//     '  Kit Fine',
+//     ' Nelson Goodman',
+//     'David Kolb',
+//     '   Saul Kripke',
+//     '  Trenton Merricks',
+//     '  Jay Rosenberg',
+//   ]
+// ));
 
 module.exports = {
   createDreamTeam
