ddab076f27492a556eb1a2e51de92a8211262896
OrkasST <egor31@outlook.com>
2023-04-17 17:39:31 +0300

    feat: transform-array task done

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,39 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
+const controls = ['--discard-next', '--discard-prev', '--double-next', '--double-prev'];
+function transform(arr) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  if (!Array.isArray(arr)) throw new Error('\'arr\' parameter must be an instance of the Array!');
+  let resArr = [...arr];
+  for (let i = 0; i < resArr.length; i++) {
+    if(resArr[i] === '--discard-next') {
+      if (typeof resArr[i+1] === 'string') {
+        resArr.splice(i,1);
+        i--;
+        continue;
+      }
+      let el = resArr[i+2];
+      resArr.splice(i, (typeof el === 'string' && el.substring(el.length - 4) === 'prev') ? 3 : 2);
+      i--;
+      continue;
+    }
+    if (resArr[i] === '--discard-prev') {
+      i > 0 ? resArr.splice( i-1, 2) : resArr.splice(i, 1);
+      i -= 2;
+      continue;
+    }
+    if (resArr[i] === '--double-next') {
+      i + 1 < resArr.length || typeof resArr[i+1] === 'string' ? resArr.splice( i, 1, resArr[i+1]) : resArr.splice(i, 1);
+      i++;
+      continue;
+    }
+    if (resArr[i] === '--double-prev') {
+      i > 0 ? resArr.splice( i, 1, resArr[i-1]) : resArr.splice(i, 1);
+    }
+  }
+  return resArr;
 }
 
 module.exports = {
