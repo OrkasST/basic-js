887b84532c78782fc260b9681563e341f543d823
OrkasST <egor31@outlook.com>
2023-04-18 00:57:06 +0300

    feat: vigenere-cipher task done

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,15 +20,74 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(isDirect = true) {
+    this.isDirect = isDirect;
+    this.dictionary = [
+      'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+    ]
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(str, key) {
+    if (!str || !key) throw new Error("Incorrect arguments!");
+    let encrypted = str;
+    key = key.toUpperCase();
+    let spaces = [];
+    encrypted = encrypted.toUpperCase().split('')
+    .filter((el, i) => {
+      const reg = /[^a-z0-9]/gi;
+      if (reg.test(el)) {
+        spaces.push([i, el]);
+        return false;
+      } else return true;
+    })
+    .map((ltr, i) => {
+      let plus = this.dictionary.indexOf(key[i >= key.length ? i % key.length : i]);
+      let index = this.dictionary.indexOf(ltr) + plus;
+      if (this.dictionary.length <= index) index = index - this.dictionary.length;
+      return this.dictionary.indexOf(ltr) < 0 ? ltr : this.dictionary[index];
+    });
+    spaces.forEach(el => encrypted.splice(el[0], 0, el[1]));
+    if (!this.isDirect) {
+      let reverse = [];
+      encrypted.forEach(el => reverse.unshift(el));
+      encrypted = reverse;
+    }
+    return encrypted.join('');
+  }
+  decrypt(str, key) {
+    if (!str || !key) throw new Error("Incorrect arguments!");
+    let encrypted = str;
+    key = key.toUpperCase();
+    let spaces = [];
+    encrypted = encrypted.toUpperCase().split('')
+    .filter((el, i) => {
+      const reg = /[^a-z0-9]/gi;
+      if (reg.test(el)) {
+        spaces.push([i, el]);
+        return false;
+      } else return true;
+    });
+    encrypted = encrypted.map((ltr, i) => {
+      let plus = this.dictionary.indexOf(key[i >= key.length ? i % key.length : i]);
+      let index = this.dictionary.indexOf(ltr) - plus;
+      if (index < 0) index = index + this.dictionary.length;
+      return this.dictionary.indexOf(ltr) < 0 ? ltr : this.dictionary[index];
+    });
+    spaces.forEach(el => encrypted.splice(el[0], 0, el[1]));
+    if (!this.isDirect) {
+      let reverse = [];
+      encrypted.forEach(el => reverse.unshift(el));
+      encrypted = reverse;
+    }
+    return encrypted.join('');
   }
 }
+// let crypt = new VigenereCipheringMachine();
+// let cryptRev = new VigenereCipheringMachine(false);
+// console.log(crypt.encrypt('attack at dawn!', 'alphonse')); // AEIHQX SX DLLU!
+// console.log(crypt.decrypt('AEIHQX SX DLLU!', 'alphonse')); // AEIHQX SX DLLU!
+// console.log(cryptRev.encrypt('attack at dawn!', 'alphonse')); // !ULLD XS XQHIEA
+// console.log(cryptRev.decrypt('AEIHQX SX DLLU!', 'alphonse')); // !NWAD TA KCATTA
+
 
 module.exports = {
   VigenereCipheringMachine
